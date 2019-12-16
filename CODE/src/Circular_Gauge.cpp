09806dc5dd946f698b0820cc0bfab75131cbbc84
Morten Brask Jensen <mimanex.dk@gmail.com>
2019-12-16 15:34:48 +0100

    project update lib deps

--- CODE/src/Circular_Gauge.cpp
+++ CODE/src/Circular_Gauge.cpp
@@ -1,7 +1,7 @@
 #include "SPI.h"
 #include "Wire.h"
-#include "Adafruit_GFX.h"
-#include "Adafruit_SSD1306.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include "Circular_Gauge.h"
 
 extern Adafruit_SSD1306 display;
